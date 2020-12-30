5c274321f2f2091048f22adb7cbf74d713c40d98
huntermitchell123 <63325120+huntermitchell123@users.noreply.github.com>
2020-12-29 23:44:35 -0800

    Update compareYourself.py

--- compareYourself.py
+++ compareYourself.py
@@ -1,66 +1,97 @@
-### Code that returns image most similar to your input image from list of scraped google images
-### Written by Hunter Mitchell - 4/7/20 
+"""
+Hunter Mitchell
+12/29/20
 
-### Note: Must have chromedriver executable set up in existing path, along with Pillow, Selenium, and Skimage
+Description: Scrapes images from Google based on a search input, compares each image to an input image on your machine,
+saves the image that is the most structurally similar, and then shows both of them
+
+Packages Required: Pillow, scikit-image, opencv-python, selenium, webdriver-manager
+Note: Must have chromedriver compatible with your chrome version downloaded -> https://chromedriver.chromium.org/downloads
+"""
+
 
 ### SSIM Testing imports
 from skimage.measure import compare_ssim
 from PIL import Image
-import argparse
-import imutils
 import cv2
 
 ### Webscraping imports
 from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
 from webdriver_manager.chrome import ChromeDriverManager
-from PIL import Image
 
-import json
+### Other imports
 import os
 import io
 import requests
-import urllib
-import argparse
 import time
 import hashlib
 
 
-### FILL OUT CHROMEDRIVER PATH HERE ###
-DRIVER_PATH = '/Users/huntermitchell/Desktop/chromedriver 2'
-wd = webdriver.Chrome(executable_path=DRIVER_PATH)
-#######################################
 
-### ENTER STUFF HERE ###
-own_path = '/Users/huntermitchell/Downloads/hunter.jpeg'
-search_term = 'cat'
-number_of_images = 20
-########################
+#-------------------------------#
+# FILL OUT SPECIFICATIONS HERE  #
+#-------------------------------#
+DRIVER_PATH = '/Users/huntermitchell/Documents/PYTHON_FILES/chromedriver' # chromedriver executable path
+MY_IMAGE_PATH = '/Users/huntermitchell/Desktop/HunterPix/bandanaCut.jpg' # path to image you want to compare (Note: This image will be resized)
+SEARCH_TERM = 'actor' # google search term you want to find images based on
+NUMBER_OF_IMAGES = 100 # how many images to get
+TARGET_PATH = '/Users/huntermitchell/Documents/PYTHON_FILES/compareYourself' # location to store final image
 
 
-### Function for calculating SSIM between two images. Adaptd from https://github.com/mostafaGwely/Structural-Similarity-Index-SSIM-
+
+
 def compareImages(path1,path2):
-    imageA = cv2.imread(path1)
-    imageB = cv2.imread(path2)
-    (score, diff) = compare_ssim(imageA, imageB, full=True, multichannel=True)
-    diff = (diff * 255).astype("uint8")
-    print("SSIM: {}".format(score))
-    return score
+    """
+    Calculates structural similarity between two images. Adapted from
+    https://github.com/mostafaGwely/Structural-Similarity-Index-SSIM-
 
-### Function to resize image to 200 x 200 pixels, since images must be the same size to compare
+    Parameters:
+    path1,path2: filepaths of image locations to compare
+    
+    Return:
+    score between 0 and 1 representing how similar the images are
+    """
+    img1 = cv2.imread(path1)
+    img2 = cv2.imread(path2)
+    try:
+        (score, diff) = compare_ssim(img1, img2, full=True, multichannel=True)
+        diff = (diff * 255).astype("uint8")
+        print("SSIM: {}".format(score))
+        return score
+    except:
+        return 0.0
+
+
+
 def resizeImage(path):
+    """
+    Resizes image to 200x200 pixels
+
+    Parameters:
+    path: filepath to image location
+    """
     temp_pic = Image.open(path,mode='r')
     temp_pic = temp_pic.resize((200,200))
     temp_pic.save(path)
 
 
-### Functions to scrape images from web. Adapted from https://towardsdatascience.com/image-scraping-with-python-a96feda8af2d
 
-### Function to get the image urls based on search
-def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_between_interactions:int=1):
+def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_between_interactions:float=0.5):
+    """
+    Gets list of image urls to download
+    Adapted from https://towardsdatascience.com/image-scraping-with-python-a96feda8af2d
+
+    Parameters:
+    query: search term string
+    max_links_to_fetch: number of links to get
+    wd: webdriver object
+    sleep_between_interactions: time to wait between getting urls (default 0.5)
+
+    Returns: list of image urls
+    """
     def scroll_to_end(wd):
         wd.execute_script("window.scrollTo(0, document.body.scrollHeight);")
-        time.sleep(sleep_between_interactions)    
+        time.sleep(sleep_between_interactions)
     
     # build the google query
     search_url = "https://www.google.com/search?safe=off&site=&tbm=isch&source=hp&q={q}&oq={q}&gs_l=img"
@@ -113,8 +144,18 @@ def fetch_image_urls(query:str, max_links_to_fetch:int, wd:webdriver, sleep_betw
     return image_urls
 
 
-### Function to download images into folder
+
 def persist_image(count,path_list,folder_path:str,url:str):
+    """
+    Downloads images from image url list into a folder
+    Also adapted from https://towardsdatascience.com/image-scraping-with-python-a96feda8af2d 
+
+    Parameters:
+    count: which image number it is on
+    path_list: list of paths where images will go
+    folder_path: Local path of folder that images will be stored in
+    url: URL of image to download
+    """
     try:
         image_content = requests.get(url).content
 
@@ -130,60 +171,61 @@ def persist_image(count,path_list,folder_path:str,url:str):
         with open(file_path, 'wb') as f:
             image.save(f, "JPEG", quality=85)
         print(f"SUCCESS - saved {url} - as {file_path}")
-        path_list[count] = file_path 
+        path_list[count] = file_path
     except Exception as e:
         print(f"ERROR - Could not save {url} - {e}")
 
 
-### Function that loops through and puts all downloaded images in folder
-def search_and_download(path_list,search_term:str,driver_path:str,target_path='./scraped_images',number_images=5):
-    target_folder = os.path.join(target_path,'_'.join(search_term.lower().split(' ')))
+
+def main():
+
+    wd = webdriver.Chrome(executable_path=DRIVER_PATH)
+
+    # resize input image and create empty file paths list
+    resizeImage(MY_IMAGE_PATH)
+    file_paths = ['']*NUMBER_OF_IMAGES
+
+    os.chdir(TARGET_PATH)
+
+    target_folder = os.path.join('./scraped_images','_'.join(SEARCH_TERM.lower().split(' ')))
 
     if not os.path.exists(target_folder):
         os.makedirs(target_folder)
-
-    with webdriver.Chrome(executable_path=driver_path) as wd:
-        res = fetch_image_urls(search_term, number_images, wd=wd, sleep_between_interactions=0.5)
-        
+    
+    # get list of image urls to download
+    res = fetch_image_urls(SEARCH_TERM, NUMBER_OF_IMAGES, wd=wd, sleep_between_interactions=0.25)
+    
+    # download each of them
     count = 0
     for elem in res:
-        persist_image(count,path_list,target_folder,elem)
-        count = count + 1
+        persist_image(count,file_paths,target_folder,elem)
+        count += 1
+
+    # go through each image, calculates structural similarity and keeps top one
+    top_image_score = 0.0
+    top_path = ''
+    for path in file_paths:
+        resizeImage(path)
+        temp = compareImages(MY_IMAGE_PATH,path)
+        if (temp > top_image_score):
+            top_image_score = temp
+            top_path = path
+
+    # delete all the other images
+    for path in file_paths:
+        if path != top_path:
+            os.remove(path)
+
+    # prints and shows original image and most similar image
+    print(f"Top image path: {top_path} with score of: {top_image_score}")
+    final_image = Image.open(top_path,mode='r')
+    final_image.show()
+    my_image = Image.open(MY_IMAGE_PATH,mode='r')
+    my_image.show()
+
+    wd.quit()
 
 
 
-
-# resize input image and create empty file paths list
-resizeImage(own_path)
-file_paths = ['']*number_of_images
-
-
-
-search_and_download(file_paths,search_term=search_term,driver_path=DRIVER_PATH,number_images=number_of_images)
-
-
-### Goes through each path, calculates similarity and keeps top one
-top_image_score = 0
-top_path = ''
-for path in file_paths:
-    resizeImage(path)
-    temp = compareImages(own_path,path)
-    if (temp > top_image_score):
-        top_image_score = temp
-        top_path = path
-
-
-# delete all the other ones
-for path in file_paths:
-    if path != top_path:
-        os.remove(path)
-
-
-# prints and shows original image and most similar image
-print('Top image path: ',top_path,' with score of: ',top_image_score)
-final_image = Image.open(top_path,mode='r')
-final_image.show()
-own_image = Image.open(own_path,mode='r')
-own_image.show()
-
-wd.quit()
+if __name__=="__main__":
+    main()
